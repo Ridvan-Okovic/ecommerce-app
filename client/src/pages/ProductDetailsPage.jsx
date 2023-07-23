70b84a203180f584fd30a54a04262df4d77fc466
Ridvan-Okovic <okovic.ridvan@gmail.com>
2023-07-23 22:54:41 +0200

    Adding product details page

--- client/src/pages/ProductDetailsPage.jsx
+++ client/src/pages/ProductDetailsPage.jsx
@@ -25,7 +25,18 @@ const ProductDetailsPage = () => {
 
   return (
     <div className="w-full h-[calc(100%-56px)] grid place-items-center">
-      Details
+      <div className="w-1/4 shadow-lg rounded flex">
+        <img
+          className="object-contain w-96 rounded-l"
+          src={product.image}
+          alt="img"
+        />
+        <div className="w-full">
+          <h3 className="text-center text-xl capitalize font-medium mt-4">
+            {product.name}
+          </h3>
+        </div>
+      </div>
     </div>
   );
 };
